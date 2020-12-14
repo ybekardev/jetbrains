c658a2fbb8cf4ad5643a97983b1c4f9eaf5beffe
Spartan <y_bekar@hotmail.com>
2020-12-13 19:49:44 -0600

    practices added

--- src/main/java/org/jetBrains/designpatterns/singleton/Singleton.java
+++ src/main/java/org/jetBrains/designpatterns/singleton/Singleton.java
@@ -3,7 +3,9 @@ package org.jetBrains.designpatterns.singleton;
 public class Singleton {
 
     //The pattern ensures that there is only one instance of the class and provides global access to it for the outer world.
-    private static volatile Singleton instance = null; // volatile guarantees the visibility for other threads of writes to that variable // volatile keyword here makes sure that the changes made in one thread are immediately reflect in other thread
+    //the keyword volatile is used for visibility. To make visible changes of a value made by one thread to other threads, we should declare the field with the keyword volatile.
+    // volatile keyword here makes sure that the changes made in one thread are immediately reflect in other thread
+    private static volatile Singleton instance = null; // volatile guarantees the visibility for other threads of writes to that variable
     private Singleton(){ }
 
         public static synchronized Singleton getInstance(){ //if you add sync in the method name it will be thread-safe
