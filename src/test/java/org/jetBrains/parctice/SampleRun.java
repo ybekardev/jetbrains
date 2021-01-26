eb7d77544f21ab7aac605ef14dec06df550bb7a9
ybekardev <y_bekar@hotmail.com>
2021-01-25 20:57:16 -0600

    circleci.yml added

---
+++ src/test/java/org/jetBrains/parctice/SampleRun.java
@@ -0,0 +1,18 @@
+package org.jetBrains.parctice;
+
+import org.junit.jupiter.api.Test;
+
+public class SampleRun {
+    @Test
+    public void testing(){
+        System.out.println("Sample method 1");
+    }
+    @Test
+    public void testing2(){
+        System.out.println("Sample method 2");
+    }
+    @Test
+    public void testing3(){
+        System.out.println("Sample method 3");
+    }
+}
