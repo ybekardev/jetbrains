5e5558e0cfd9484546098af7c2b69047a4788824
ybekardev <y_bekar@hotmail.com>
2021-02-04 11:33:35 -0600

    algorithm added

--- src/main/java/org/jetBrains/algorithms/CountNonDuplicatesInArray.java
+++ src/main/java/org/jetBrains/algorithms/CountNonDuplicatesInArray.java
@@ -1,5 +1,9 @@
 package org.jetBrains.algorithms;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class CountNonDuplicatesInArray {
     /*# Remove Duplicates from Sorted Array
 # Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
@@ -11,7 +15,11 @@ public class CountNonDuplicatesInArray {
 
     public static void main(String[] args) {
         System.out.print(removeDuplicates(new int[] {0,0,1,1,1,2,2,3,3,4}));
+        removeDup(new int[] {0,0,1,1,1,2,2,3,3,4});
+        int[] arr = new int[] {0,0,1,1,1,2,2,3,3,4};
+        Arrays.stream(arr).distinct().forEach(n -> System.out.println(n));
     }
+
     public static int removeDuplicates(int[] nums) {
         int count = 1;
         //in case of 1st and 2nd index is equal it will skip counting since they are equal it will not enter the loop
@@ -24,7 +32,18 @@ public class CountNonDuplicatesInArray {
         }
         System.out.print("\nThe length is: ");
        // System.out.println(count);
-
         return count;
+    }
+
+    public static Set removeDup(int[] num){
+        Set<Integer> nonDup = new HashSet<>();
+        for(int i = 0; i < num.length; i++){
+            //Set works like this -> if(!nonDup.contains(num[i])) then add them.
+                nonDup.add(num[i]);
+        }
+        System.out.println();
+        System.out.println("Non Duplicated: " + nonDup);
+        System.out.println("Counter: " + nonDup.size());
+        return nonDup;
     }
 }
